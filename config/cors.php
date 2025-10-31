ea297fd5bc407eaa00b2aa0dd83ee286a4b0cf64
prast13 <dwiprst13@gmail.com>
2025-11-01 06:17:08 +0700

    Add New URL allowed

--- config/cors.php
+++ config/cors.php
@@ -26,7 +26,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5174')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5174'),
+        env('FRONTEND_URL_SECONDARY', 'http://localhost:5173'),
+
+    ],
 
     'allowed_origins_patterns' => [],
 
